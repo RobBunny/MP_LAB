7c620a3f2196ddd0a972b6d7cd068ed3be48117e
RobBunny <167717462+RobBunny@users.noreply.github.com>
2025-09-24 12:48:01 +0600

    Added lab 3
    Implement a program to reverse a scanned number.

---
+++ LAB3/task_1.asm
@@ -0,0 +1,57 @@
+;Task 1:
+;Scan a variable n. Print the reverse of the number.(Example: 123 → 321)
+
+extern	printf		
+extern	scanf		
+
+SECTION .data		
+
+a:	dq	0	
+b:  dq  0
+
+enter:	db "Enter a number: ",0
+out_fmt:	db "%d", 10, 0
+out_fmt_2:	db "%s",10,0	
+in_fmt:		db "%d",0
+SECTION .text
+
+global main		
+main:				
+        push    rbp	
+        
+        mov rax,0
+        mov rdi,out_fmt_2
+        mov rsi,enter
+        call printf
+        
+        mov rax, 0
+	    mov rdi, in_fmt
+	    mov rsi, a
+	    call scanf	
+	
+	    mov	rax,[a]
+        mov rbx,0
+    loop_start:
+
+        xor rdx,rdx
+        mov rcx,10
+        div rcx
+        imul rbx, rbx, 10
+        add rbx, rdx
+	    cmp rax, 0
+        je finish
+        jmp loop_start
+
+    finish:
+	    mov	[b],rbx	
+	    mov	rdi,out_fmt		
+	    mov	rsi,[b]                  
+	    mov	rax,0		
+        call    printf
+        jmp end		
+
+    end:
+	    pop	rbp		
+
+	    mov	rax,0		
+	    ret				
